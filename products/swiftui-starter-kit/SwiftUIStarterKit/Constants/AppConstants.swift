3cd57409844f595207167c95c8b61b7f1ac143db
Aulia Vika Wulandari <avika@Mac-mini-Aulia.local>
2026-04-11 18:05:04 +0700

    feat(swiftui-starter-kit): add constants and extensions

---
+++ products/swiftui-starter-kit/SwiftUIStarterKit/Constants/AppConstants.swift
@@ -0,0 +1,7 @@
+import Foundation
+
+enum AppConstants {
+    static let apiBaseURL = "https://api.example.com/v1"
+    static let appName = "SwiftUI Starter Kit"
+    static let minPasswordLength = 8
+}
---
+++ products/swiftui-starter-kit/SwiftUIStarterKit/Constants/StorageKeys.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+enum StorageKeys {
+    static let authToken = "auth_token"
+    static let refreshToken = "refresh_token"
+    static let hasSeenOnboarding = "has_seen_onboarding"
+    static let selectedTheme = "selected_theme"
+    static let notificationsEnabled = "notifications_enabled"
+}
